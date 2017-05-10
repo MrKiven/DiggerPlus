ad8d070f46a239e59571cfa8966e2c052d3b1f03
kiven <kiven.mr@gmail.com>
2017-05-10 15:35:33 +0800

    Enhance `to_dict`

--- diggerplus/models/todo.py
+++ diggerplus/models/todo.py
@@ -19,10 +19,3 @@ class TODOModel(Model):
     @classmethod
     def get_by_title(cls, title):
         return cls.query.filter(cls.title==title).first()
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'is_done': self.is_done
-        }
